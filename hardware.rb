1b91399a3aa47203745c22e1b00d0e93cad47218
Misty De Meo <mistydemeo@gmail.com>
2013-03-23 14:25:32 -0500

    rdoc sample

--- hardware.rb
+++ hardware.rb
@@ -1,7 +1,19 @@
 require 'hardware_compat'
 
 class Hardware
+  ##
+  # = Hardware
+  # 
+  # The +Hardware+ module, and its classes, provide information about the
+  # hardware on which it is being run. This is used internally by
+  # Homebrew, and can also be used in formulae.
+  # 
+  # Hardware and its classes are extended with platform-specific modules,
+  # such as MacCPUs.
+
   module CPU extend self
+    ##
+    # Provides methods to query properties of the CPU in the computer.
     def type
       @type || :dunno
     end
@@ -38,6 +50,9 @@ class Hardware
     raise "The system `#{`uname`.chomp}' is not supported."
   end
 
+  ##
+  # Returns a string representing the number of CPU cores in English,
+  # suitable for use in user-readable output.
   def self.cores_as_words
     case Hardware.processor_count
     when 1 then 'single'
--- mac/hardware.rb
+++ mac/hardware.rb
@@ -1,3 +1,10 @@
+##
+# :title: Hardware::CPU
+# 
+# Provides methods to query properties of the CPU in the computer.
+# Several of these methods use info spewed out by sysctl.
+# Look in <mach/machine.h> for decoding info.
+
 module MacCPUs
   OPTIMIZATION_FLAGS = {
     :penryn => '-march=core2 -msse4.1',
@@ -8,10 +15,14 @@ module MacCPUs
     :g4e => '-mcpu=7450',
     :g5 => '-mcpu=970'
   }
+  ##
+  # Returns a hash containing optimization flags suitable for the
+  # hardware being used. The keys are in the same format as
+  # +CPU.family+.
   def optimization_flags; OPTIMIZATION_FLAGS.dup; end
 
-  # These methods use info spewed out by sysctl.
-  # Look in <mach/machine.h> for decoding info.
+  ##
+  # Returns :intel or :ppc.
   def type
     @type ||= `/usr/sbin/sysctl -n hw.cputype`.to_i
     case @type
@@ -24,6 +35,12 @@ module MacCPUs
     end
   end
 
+  ##
+  # Returns a symbol representing the family of CPU being used.
+  # Values are symbols, suitable for use with the +optimization_flags+
+  # hash.
+  # If the CPU family isn't recognized but is of a recognized CPU type,
+  # returns :dunno. Otherwise returns nil.
   def family
     if type == :intel
       @intel_family ||= `/usr/sbin/sysctl -n hw.cpufamily`.to_i
@@ -62,10 +79,14 @@ module MacCPUs
     end
   end
 
+  ##
+  # The number of CPU cores available, in integer form.
   def cores
     @cores ||= `/usr/sbin/sysctl -n hw.ncpu`.to_i
   end
 
+  ##
+  # Returns 32 or 64.
   def bits
     return @bits if defined? @bits
 
@@ -73,20 +94,32 @@ module MacCPUs
     @bits ||= is_64_bit ? 64 : 32
   end
 
+  ##
+  # Returns true if the AltiVec instruction set is available.
+  # This will be true on PowerPC processors G4 and newer.
   def altivec?
     type == :ppc && family != :g3
   end
 
+  ##
+  # Returns true if the SSE3 instruction set is available.
+  # This will be true on all supported Intel processors.
   def sse3?
     type == :intel
   end
 
+  ##
+  # Returns true if the SSE4 instruction set is available.
+  # This will be true on Intel processors newer than Core 2.
   def sse4?
     type == :intel && (family != :core && family != :core2)
   end
 
   protected
 
+  ##
+  # Returns a boolean representing the value obtained from the
+  # +sysctl+ kernel state tool.
   def sysctl_bool(property)
     result = nil
     IO.popen("/usr/sbin/sysctl -n #{property} 2>/dev/null") do |f|
